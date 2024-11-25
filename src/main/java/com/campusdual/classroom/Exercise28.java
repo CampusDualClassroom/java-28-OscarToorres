e4f65b4fe6029e86d4289928db1f1759bffa9cbb
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2024-11-25 08:52:47 +0100

    Ejercicio 28 completado

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -1,7 +1,14 @@
 package com.campusdual.classroom;
 
 public class Exercise28 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+
+        try {
+            int[] numero = new int[1];
+            System.out.println(numero[3]);
+        } catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
 
     }
 }
